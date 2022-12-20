72c66462d0ac8195fae0d687da99e6463bae6494
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2022-12-20 10:56:21 -0300

    Initial Commit

---
+++ SwiftUI_Animation-Star/SwiftUI_Animation_StarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Animation_StarApp.swift
+//  SwiftUI_Animation-Star
+//
+//  Created by Luiz Araujo on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Animation_StarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
