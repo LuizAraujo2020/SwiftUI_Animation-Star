fc61b56fce88f3ba1f9d3f36d4ba4f086d461b67
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2022-12-20 12:24:09 -0300

    chore: the star is changing to the filled version after completed

--- SwiftUI_Animation-Star/ContentView.swift
+++ SwiftUI_Animation-Star/ContentView.swift
@@ -15,49 +15,66 @@ struct ContentView: View {
     @State var resetStrokes: Bool   = true
     @State var strokeStart: CGFloat = 0
     @State var strokeEnd: CGFloat   = 0
+    @State var animationEnded: Bool = false
     
     // MARK:- views
     var body: some View {
-        ZStack {
-            Color.black
-                .edgesIgnoringSafeArea(.all)
-            StarShape()
-                .stroke(style: StrokeStyle(lineWidth: 2.5, lineCap: .round, lineJoin: .round, miterLimit: 5))
-                .foregroundColor(Color.white)
-                .opacity(0.0)
-            
-            StarShape()
-                .trim(from: strokeStart, to: strokeEnd)
-                .stroke(style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .round, miterLimit: 10))
-                .foregroundColor(Color.white)
-                .onAppear() {
-                    Timer.scheduledTimer(withTimeInterval: 0.35, repeats: true) { timer in
-                        if (self.strokeEnd >= 1) {
-                            if (self.resetStrokes) {
-                                Timer.scheduledTimer(withTimeInterval: 1, repeats: false) { _ in
-                                    self.strokeEnd = 0
-                                    self.strokeStart = 0
-                                    self.resetStrokes.toggle()
+        if animationEnded {
+            ZStack {
+//                Color.black
+//                    .edgesIgnoringSafeArea(.all)
+                StarShape()
+                    .fill(.yellow)
+            }
+        } else {
+            ZStack {
+//                Color.black
+//                    .edgesIgnoringSafeArea(.all)
+                StarShape()
+                    .stroke(style: StrokeStyle(lineWidth: 2.5, lineCap: .round, lineJoin: .round, miterLimit: 5))
+                    .foregroundColor(Color.white)
+                    .opacity(0.0)
+                
+                StarShape()
+                    .trim(from: strokeStart, to: strokeEnd)
+                    .stroke(style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .round, miterLimit: 10))
+                    .foregroundColor(Color.yellow)
+                    .onAppear() {
+                        var counter = 0
+                        Timer.scheduledTimer(withTimeInterval: 0.35, repeats: true) { timer in
+                            if (self.strokeEnd >= 1) {
+                                if (self.resetStrokes) {
+                                    Timer.scheduledTimer(withTimeInterval: 1, repeats: false) { _ in
+                                        self.strokeEnd = 0
+                                        self.strokeStart = 0
+                                        self.resetStrokes.toggle()
+                                    }
+                                    self.resetStrokes = false
                                 }
-                                self.resetStrokes = false
+                            }
+                            counter += 1
+                            withAnimation(Animation.easeOut(duration: 0.5)) {
+                                self.strokeEnd += 0.1
+                                self.strokeStart = self.strokeEnd - 1.5
+                                
+                                // Gets the end of the animation
+                                if (counter == 11) {
+                                    print("ACABIOU")
+                                    animationEnded = true
+                                }
                             }
                         }
-                        withAnimation(Animation.easeOut(duration: 0.5)) {
-                            self.strokeEnd += 0.1
-                            self.strokeStart = self.strokeEnd - 1.5
-                        }
                     }
-                    print("Acabou")
-                }
-            VStack{
-                Spacer()
-                HStack {
+                VStack{
                     Spacer()
-                    Text("@Shubham_iosdev")
-                        .foregroundColor(.white)
-                        .font(.system(size: 28, weight: .medium, design: .monospaced))
-                        .opacity(0.3)
-                }.padding(.trailing, 16)
+                    HStack {
+                        Spacer()
+                        Text("@Shubham_iosdev")
+                            .foregroundColor(.white)
+                            .font(.system(size: 28, weight: .medium, design: .monospaced))
+                            .opacity(0.3)
+                    }.padding(.trailing, 16)
+                }
             }
         }
     }
